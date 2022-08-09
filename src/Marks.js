cb7b6ef6eea29194605d141fa073754d91a0795e
barbara <barbara@bmoah001@gmail.com>
2022-08-09 16:50:21 +0200

    added spin removed touch

--- src/Marks.js
+++ src/Marks.js
@@ -1,6 +1,6 @@
 import { geoOrthographic, geoPath, geoGraticule, append } from 'd3';
 import React, {useState, useCallback, useEffect} from 'react';
-import $ from 'jquery';
+import $, { data } from 'jquery';
 
 const width = 500;
 const height = 500;
@@ -9,10 +9,20 @@ const intialMousePosition = {x: width/2, y: height/2};
 const projection = geoOrthographic();
 const path = geoPath(projection);
 const graticule = geoGraticule();
+const rotate = [0, 0];
+const velocity = [.015, -0];
+
 
 
 export const Marks = ({data: {land, geometry}}) => {
 
+const time = Date.now();
+
+
+ 
+  const [now, setNow] = useState(Date.now())
+  const [dt, setDT] = useState(Date.now() - now)
+ 
   const [MousePosition, SetMousePosition] = useState(intialMousePosition)
   const [mouseDown, SetMousedDown] = useState(false)
 
@@ -30,6 +40,13 @@ const handleMouseUp = useCallback((event) => {
   
 },[])
 
+const handleClick = useCallback((event) => {
+ 
+
+console.log(event);  
+},[])
+
+
 const handleMouseMove = useCallback((event) => {
   
   const {clientX, clientY}= event;
@@ -42,21 +59,21 @@ const handleMouseMove = useCallback((event) => {
   
 },[SetMousePosition, mouseDown])
 
+
 useEffect(() => {
-  // Update the document title using the browser API
-  geometry.features.forEach(feature => {
+  const interval = setInterval(() => {
+    setDT(Date.now() - now);
+   
+  }, 300);
 
-    console.log(feature.properties.name, );
-  });
+  return () => clearInterval(interval); // This represents the unmount function, in which you need to clear your interval to prevent memory leaks.
+}, [])
  
-},[]);
-
-
 
   
   return(
   <g  className="marks" onMouseDown = {handleMouseDown} onMouseMove = {handleMouseMove} onMouseUp= {handleMouseUp}>
-      {projection.rotate([MousePosition.x + 30 / 60, -MousePosition.y, 0])}
+      {projection.rotate([rotate[0] + velocity[0] * dt, rotate[1] + velocity[1] * dt])}
      <path className = "sphere" d={ path({type: 'Sphere'})}/>
       <path className = "graticule" d={ path(graticule())} style = {{display: 'none'}}/> 
      
@@ -68,14 +85,30 @@ useEffect(() => {
 
 {
       geometry.features.map((feature, i) => (
-       <path className = "interiors" d={ path(feature)} key={i}>
-        <text >{feature.properties.name}</text> 
+        <g>
+
+       <path className = "interiors" d={ path(feature)} key={feature.properties.name} id={feature.properties.name}>
+       {/* <div     cx={path.centroid(feature)[0]} cy={path.centroid(feature)[1]}   fontSize="36">{feature.properties.name}</div> */}
+
        </path>
-   
-      )) 
+ 
+       <circle  className = "interiors"   cx={path.centroid(feature)[0]} cy={path.centroid(feature)[1]}  r="2"  fill="red" fontSize="36"></circle>
+      
+
+ 
+       </g>
+
+
+
+        )) 
     }
+
+ 
+
  
  
+ 
+
 
        
   
